05aa37ffb9ce6bbbda48880dd0e18862ac10de20
Sam Naseri <sam.n@live.com.au>
2018-04-04 16:40:06 +1000

    Removed extra code from ReceiveAssetViewModel

--- Agate.Business/ViewModels/Main/ReceiveAssetViewModel.cs
+++ Agate.Business/ViewModels/Main/ReceiveAssetViewModel.cs
@@ -6,25 +6,15 @@ namespace Agate.Business.ViewModels.Main
     {
         public ReceiveAssetViewModel()
         {
-            
         }
 
         public void Initialize(AssetHomeViewModel parent)
         {
-            // todo : make sure we have a deposit address as it might be first time user using app and userasset read from file does not have address
-            // so show a "Generating address ..." or not allow user to come to this page at all
-
             Parent = parent;
             DepositAddress = parent.UserAsset.CurrentReceiveAddress;
-
-            GenerateNewAddressCommand = new XCommand(GenerateNewAddress);
         }
+
         public AssetHomeViewModel Parent { get; set; }
         public string DepositAddress { get; set; }
-        public IXCommand GenerateNewAddressCommand { get; set; }
-        public void GenerateNewAddress()
-        {
-            // todo : uncomment XAML part and implement here
-        }
     }
 }
