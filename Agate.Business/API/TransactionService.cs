b2b5d7e865498d6665658aea810b3b6e0fe6730f
Sam Naseri <sam.n@live.com.au>
2018-04-15 11:05:36 +1000

    transactions API don't need to have assetId passed in as a query parameter

--- Agate.Business/API/TransactionService.cs
+++ Agate.Business/API/TransactionService.cs
@@ -6,12 +6,12 @@ namespace Agate.Business.API
     public class TransactionService : ITransactionService
     {
         public async Task<GenerateDepositAddressResponse> GenerateDepositAddress(GenerateDepositAddressRequest request) =>
-            await Client.Post<GenerateDepositAddressRequest, GenerateDepositAddressResponse>($"transactions/{request.AssetId}/generateDepositAddress", request);
+            await Client.Post<GenerateDepositAddressRequest, GenerateDepositAddressResponse>($"transactions/generateDepositAddress", request);
 
         public async Task<SendOrderResponse> SendOrder(SendOrderRequest request) =>
-            await Client.Post<SendOrderRequest, SendOrderResponse>($"transactions/{request.AssetId}/sendOrder", request);
+            await Client.Post<SendOrderRequest, SendOrderResponse>($"transactions/sendOrder", request);
 
         public async Task<TransferOrderResponse> TransferOrder(TransferOrderRequest request) =>
-            await Client.Post<TransferOrderRequest, TransferOrderResponse>($"transactions/{request.AssetId}/transferOrder", request);
+            await Client.Post<TransferOrderRequest, TransferOrderResponse>($"transactions/transferOrder", request);
     }
 }
