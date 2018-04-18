ade122828dcddd553ae29c74791fb5666198c8e9
Sam Naseri <sam.n@live.com.au>
2018-04-18 12:56:11 +1000

    Added two new menu items (Trader Bot and AI Engine)

--- Agate.Business/ViewModels/Main/MainMenuViewModel.cs
+++ Agate.Business/ViewModels/Main/MainMenuViewModel.cs
@@ -12,6 +12,8 @@ namespace Agate.Business.ViewModels.Main
             AllMenuItems = new List<MenuItem>(new[]{
                 new MenuItem("Manage Assets", typeof(object), "#921243", "\ue90f"),
                 new MenuItem("Manage Cards", typeof(object), "#921243", "\ue870"),
+                new MenuItem("Trader Bot", typeof(object), "#921243", "\ue80c"),
+                new MenuItem("AI Engine", typeof(object), "#921243", "\ue906"),
                 new MenuItem("Settings", typeof(object), "#921243", "\ue90f"),
                 new MenuItem("Tools", typeof(object), "#921243", "\ue8b8"),
                 new MenuItem("Help Center", typeof(object), "#921243", "\ue887"),
